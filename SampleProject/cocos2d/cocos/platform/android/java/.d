93d61578dc7dbfcc5b030081d9d74fcd6c0669d6
Ichiro Dohi <ichi-dohi@i-mobile.co.jp>
2017-05-08 14:03:17 +0900

    Android Studio用にmkファイル修正。

---
+++ SampleProject/cocos2d/cocos/platform/android/java/.d
@@ -0,0 +1,4 @@
+/Users/maio/Documents/workspace/_cocos/_git/maio-Cocos2d-x-Plugin/SampleProject/cocos2d/cocos/platform/android/java/gen/com/enhance/gameservice/IGameTuningService.java : \
+  /Users/maio/Documents/workspace/_cocos/_git/maio-Cocos2d-x-Plugin/SampleProject/cocos2d/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl
+
+/Users/maio/Documents/workspace/_cocos/_git/maio-Cocos2d-x-Plugin/SampleProject/cocos2d/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl :
