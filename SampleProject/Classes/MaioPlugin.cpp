712c81c05b9e802dd4dad06ff4a728c2fbd0abc5
Ichiro Dohi <ichi-dohi@i-mobile.co.jp>
2017-05-09 15:07:39 +0900

    レスポンスを受け取れるよう実装。

--- SampleProject/Classes/MaioPlugin.cpp
+++ SampleProject/Classes/MaioPlugin.cpp
@@ -14,9 +14,15 @@
 
 static maio::MaioListener *_listener;
 
-// extern "C" {
-//     JNIEXPORT void Java_jp_maio_sdk_plugin_cocos_android_MaioPlugin_onInitialized(JNIEnv *, jclass, jstring);
-// }
+extern "C" {
+    JNIEXPORT void Java_jp_maio_sdk_plugin_cocos_android_MaioPlugin_onInitialized(JNIEnv *, jobject);
+    JNIEXPORT void Java_jp_maio_sdk_plugin_cocos_android_MaioPlugin_onChangedCanShow(JNIEnv *, jobject, jstring, jboolean);
+    JNIEXPORT void Java_jp_maio_sdk_plugin_cocos_android_MaioPlugin_onStartAd(JNIEnv *, jobject, jstring);
+    JNIEXPORT void Java_jp_maio_sdk_plugin_cocos_android_MaioPlugin_onFinishedAd(JNIEnv *, jobject, jstring, jint, jboolean, jstring);
+    JNIEXPORT void Java_jp_maio_sdk_plugin_cocos_android_MaioPlugin_onClickedAd(JNIEnv *, jobject, jstring);
+    JNIEXPORT void Java_jp_maio_sdk_plugin_cocos_android_MaioPlugin_onClosedAd(JNIEnv *, jobject, jstring);
+    JNIEXPORT void Java_jp_maio_sdk_plugin_cocos_android_MaioPlugin_onFailed(JNIEnv *, jobject, jstring, jint);
+}
 
 // callback from Java
 JNIEXPORT void Java_jp_maio_sdk_plugin_cocos_android_MaioPlugin_onInitialized(JNIEnv *env, jobject thiz) {
@@ -25,6 +31,63 @@ JNIEXPORT void Java_jp_maio_sdk_plugin_cocos_android_MaioPlugin_onInitialized(JN
     }
 }
 
+JNIEXPORT void Java_jp_maio_sdk_plugin_cocos_android_MaioPlugin_onChangedCanShow(JNIEnv *env, jobject thiz, jstring jzoneId, jboolean newValue){
+    if(_listener != nullptr) {
+        std::string zoneId = cocos2d::JniHelper::jstring2string(jzoneId); // jstringをstd::stringに変換
+        _listener->onChangedCanShow(zoneId.c_str(), (bool)newValue);
+    }
+}
+
+JNIEXPORT void Java_jp_maio_sdk_plugin_cocos_android_MaioPlugin_onStartAd(JNIEnv *env, jobject thiz, jstring jzoneId) {
+    if(_listener != nullptr) {
+        std::string zoneId = cocos2d::JniHelper::jstring2string(jzoneId); // jstringをstd::stringに変換
+        _listener->onStartAd(zoneId.c_str());
+    }
+}
+
+JNIEXPORT void Java_jp_maio_sdk_plugin_cocos_android_MaioPlugin_onFinishedAd(JNIEnv *env, jobject thiz, jstring jzoneId, jint jplaytime, jboolean jskipped, jstring jrewardParam) {
+    if(_listener != nullptr) {
+        std::string zoneId = cocos2d::JniHelper::jstring2string(jzoneId); // jstringをstd::stringに変換
+        std::string rewardParam = cocos2d::JniHelper::jstring2string(jrewardParam); // jstringをstd::stringに変換
+        _listener->onFinishedAd(zoneId.c_str(), (int) jplaytime, (bool)jskipped, rewardParam.c_str());
+    }
+}
+
+JNIEXPORT void Java_jp_maio_sdk_plugin_cocos_android_MaioPlugin_onClickedAd(JNIEnv *env, jobject thiz, jstring jzoneId) {
+    if(_listener != nullptr) {
+        std::string zoneId = cocos2d::JniHelper::jstring2string(jzoneId); // jstringをstd::stringに変換
+        _listener->onClickedAd(zoneId.c_str());
+    }
+}
+
+JNIEXPORT void Java_jp_maio_sdk_plugin_cocos_android_MaioPlugin_onClosedAd(JNIEnv *env, jobject thiz, jstring jzoneId) {
+    if(_listener != nullptr) {
+        std::string zoneId = cocos2d::JniHelper::jstring2string(jzoneId); // jstringをstd::stringに変換
+        _listener->onClosedAd(zoneId.c_str());
+    }
+}
+
+JNIEXPORT void Java_jp_maio_sdk_plugin_cocos_android_MaioPlugin_onFailed(JNIEnv *env, jobject thiz, jstring jzoneId, jint failReason) {
+    if(_listener != nullptr) {
+        std::string zoneId = cocos2d::JniHelper::jstring2string(jzoneId); // jstringをstd::stringに変換
+        maio::FailReason failReason;
+        switch ((int)failReason) {
+            case 1: failReason = maio::FailReason::AdStockOut; break;
+            case 2: failReason = maio::FailReason::NetworkServer; break;
+            case 3: failReason = maio::FailReason::NetworkClient; break;
+            case 4: failReason = maio::FailReason::NetworkConnection; break;
+            case 5: failReason = maio::FailReason::VideoPlayback; break;
+            
+            case 0: 
+            default: 
+                failReason = maio::FailReason::Unknown; break;
+        }
+
+        _listener->onFailed(zoneId.c_str(), failReason);
+    }
+}
+
+
 // MaioPlugin methods
 void maio::MaioPlugin::start( const char *mediaId, maio::MaioListener *listener ) {
 
@@ -32,7 +95,12 @@ void maio::MaioPlugin::start( const char *mediaId, maio::MaioListener *listener 
         delete _listener;
         // &_listener = 0;
     }
-    _listener = listener;
+
+    if(listener == nullptr) {
+        _listener = new maio::MaioListener();
+    } else {
+        _listener = listener;
+    }
     
     cocos2d::JniMethodInfo methodInfo;
     if (cocos2d::JniHelper::getStaticMethodInfo(methodInfo, MAIO_CLASS, "start", "(Ljava/lang/String;)V")) 
@@ -74,10 +142,10 @@ bool maio::MaioPlugin::canShow( const char *zoneId ) {
     {
         jstring jstringZoneId = methodInfo.env->NewStringUTF(zoneId);
         jboolean jResult = methodInfo.env->CallStaticBooleanMethod(methodInfo.classID, methodInfo.methodID, jstringZoneId);
+        
         result = (bool)jResult;
 
         methodInfo.env->DeleteLocalRef(jstringZoneId);
-        // methodInfo.env->DeleteLocalRef(jResult);
         methodInfo.env->DeleteLocalRef(methodInfo.classID);
     }
 
@@ -125,7 +193,7 @@ char const* maio::MaioPlugin::getSdkVersion(){
         methodInfo.env->DeleteLocalRef(result);
         methodInfo.env->DeleteLocalRef(methodInfo.classID);
     }
-    // TODO: 本当は以下のようにダイレクトでconst char*を取ってこれたらいいなあ。
+    // TODO: 本当は以下のようにダイレクトでconst char*を取ってこれたらいいなあ。(下記は動かない)
         // jobject objResult = methodInfo.env->CallStaticObjectMethod(methodInfo.classID, methodInfo.methodID);
         // sdkVersion = methodInfo.env->GetStringUTFChars(methodInfo.env, (jstring)objResult, 0);
         // methodInfo.env->DeleteLocalRef(objResult);
--- SampleProject/proj.android-studio/app/src/jp/maio/sdk/plugin/cocos/android/MaioPlugin.java
+++ SampleProject/proj.android-studio/app/src/jp/maio/sdk/plugin/cocos/android/MaioPlugin.java
@@ -11,85 +11,94 @@ import jp.maio.sdk.android.MaioAdsListener;
 public class MaioPlugin
 {
     private static native void onInitialized();
-//    private static native void onChangedCanShow(String zoneId, boolean newValue);
-//    private static native void onStartAd(String zoneId);
-//    private static native void onFinishedAd(String zoneId, int playtime, boolean skipped, String rewardParam);
-//    private static native void onClickedAd(String zoneId);
-//    private static native void onClosedAd(String zoneId);
-//    private static native void onFailed(String zoneId, int reason);
+    private static native void onChangedCanShow(String zoneId, boolean newValue);
+    private static native void onStartAd(String zoneId);
+    private static native void onFinishedAd(String zoneId, int playtime, boolean skipped, String rewardParam);
+    private static native void onClickedAd(String zoneId);
+    private static native void onClosedAd(String zoneId);
+    private static native void onFailed(String zoneId, int reason);
 
     public static void start(String mediaId) {
         Log.d("Maio", "called start");
         MaioAds.init(Cocos2dxHelper.getActivity(), mediaId, new MaioAdsListener(){
             @Override
             public void onInitialized() {
-                Log.d("Maio", "onInitialized");
+                MaioPlugin.onInitialized();
             }
             @Override
             //変更時の処理
             public void onChangedCanShow(String zoneId, boolean newValue) {
-                Log.d("Maio", "onChangedCanShow");
+                MaioPlugin.onChangedCanShow(zoneId, newValue);
             }
             @Override
             //再生直前に呼ばれる処理
             public void onOpenAd(String zoneId) {
-                Log.d("Maio", "onOpenAd");
+                MaioPlugin.onStartAd(zoneId);
             }
             @Override
             //再生時に呼ばれる処理
             public void onStartedAd(String zoneId) {
-                Log.d("Maio", "onStartedAd");
+                // MEMO: onStartedAdはiOS側に実装がなく、onOpenAdとほぼ同タイミングの為無視
             }
             @Override
             //再生終了時に呼ばれる処理
             public void onFinishedAd(int playtime, boolean skipped, int duration, String zoneId) {
-                Log.d("Maio", "onFinishedAd");
-            }
-            @Override
-            //広告が閉じられた際の処理
-            public void onClosedAd(String zoneId) {
-                Log.d("Maio", "onClosedAd");
+                // TODO: androidではrewardParamを実装していないので、実装されたら以下の 0 を修正する
+                MaioPlugin.onFinishedAd(zoneId, playtime, skipped, "0");
             }
             @Override
             //広告クリック時に呼ばれる処理
             public void onClickedAd(String zoneId) {
-                Log.d("Maio", "onClickedAd");
+                MaioPlugin.onClickedAd(zoneId);
+            }
+            @Override
+            //広告が閉じられた際の処理
+            public void onClosedAd(String zoneId) {
+                MaioPlugin.onClosedAd(zoneId);
             }
             @Override
             //エラー時に呼ばれる処理
             public void onFailed(FailNotificationReason reason, String zoneId) {
-                Log.d("Maio", "onFailed");
+                int failReason;
+                switch (reason)
+                {
+                    case AD_STOCK_OUT       :failReason = 1; break;
+                    case RESPONSE           :failReason = 2; break;
+                    case NETWORK_NOT_READY  :failReason = 3; break;
+                    case NETWORK            :failReason = 4; break;
+                    case VIDEO              :failReason = 5; break;
+                    
+                    case UNKNOWN:
+                    default:
+                        failReason = 0; break;
+                }
+
+                MaioPlugin.onFailed(zoneId, failReason);
             }
         });
     }
 
     public static boolean canShow() {
-        Log.d("Maio", "called can show");
         return MaioAds.canShow();
     }
 
     public static boolean canShow(String zoneId) {
-        Log.d("Maio", "called can show zoneId");
         return MaioAds.canShow(zoneId);
     }
 
     public static void show() {
-        Log.d("Maio", "called show");
         MaioAds.show();
     }
 
     public static void show(String zoneId) {
-        Log.d("Maio", "called show zoneId");
         MaioAds.show(zoneId);
     }
 
     public static String getSdkVersion() {
-        Log.d("Maio", "called getSdkVersion");
         return MaioAds.getSdkVersion();
     }
 
     public static void setAdTestMode(boolean setAdTestMode) {
-        Log.d("Maio", "called setAdTestMode");
         MaioAds.setAdTestMode(setAdTestMode);
     }
 }
